bf3b4bd17250a5eb5a9f2f6762bc78248d894d26
Jacob <brewbakerjacob@gmail.com>
2026-07-08 10:57:11 -0400

    pt5 hooked up breeding symbols

--- scripts/stations/screen_cell_breeding/screen_breeding_loader/handle_display/handle_display.gd
+++ scripts/stations/screen_cell_breeding/screen_breeding_loader/handle_display/handle_display.gd
@@ -2,6 +2,8 @@ extends Node
 
 # display helper componnets
 @onready var main_cell_loader : Node = $MainCellLoader
+@onready var preview_charge_direction : Node = $PreviewChargeDirection
+@onready var symbol_manager : Node = $"../SeatCellLoading/SymbolManager"
 
 func _handle_display(
 	main_left_cell : BrainCell, 
@@ -16,11 +18,27 @@ func _handle_display(
 	main_cell_loader._handle_recieve_main_cells(
 		main_left_cell,
 		main_right_cell,
-		
-		
 	)
 	
 	# TODO : update energy boost if possible
 	
-	# TODO : update arrows if possible (NEED BOTH CELLS)
+	# update symbols
+	update_symbols(main_left_cell, main_right_cell)
+	
+
+	
+
+func update_symbols(main_left_cell : BrainCell, main_right_cell : BrainCell) :
+	if main_left_cell : 	
+		symbol_manager.check_for_checkmarks('left', main_left_cell)
+	
+	if main_right_cell : 	
+		symbol_manager.check_for_checkmarks('right', main_right_cell)
+	
+	if main_left_cell and main_right_cell :
+		symbol_manager.check_for_symbols(main_left_cell, main_right_cell)
+	
+	if not main_left_cell or not main_right_cell : 
+		symbol_manager.hide_symbols()
+	
 	
