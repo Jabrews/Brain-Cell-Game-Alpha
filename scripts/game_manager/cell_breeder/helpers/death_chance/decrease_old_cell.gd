b3d0fb474ab7cbe07fb8a88037423c10271a78bd
Jacob <brewbakerjacob@gmail.com>
2026-09-23 20:53:42 -0400

    fixed old cell stat increase

--- scripts/game_manager/cell_breeder/helpers/death_chance/decrease_old_cell.gd
+++ scripts/game_manager/cell_breeder/helpers/death_chance/decrease_old_cell.gd
@@ -4,9 +4,9 @@ func _decrease(cell : BrainCell) -> BrainCell :
 	
 	cell.life_span -= 1	 
 	
-	var strength_increase_percant = randf_range(0.5, 0.20)	
-	var intelligence_increase_percant = randf_range(0.5, 0.20)	
-	var community_increase_percant = randf_range(0.5, 0.20)	
+	var strength_increase_percant = randf_range(0.15, 0.20)	
+	var intelligence_increase_percant = randf_range(0.15, 0.20)	
+	var community_increase_percant = randf_range(0.15, 0.20)	
 	
 	
 	cell.strength.defect += IVCellCreator.max_stat_value * strength_increase_percant
@@ -16,6 +16,6 @@ func _decrease(cell : BrainCell) -> BrainCell :
 	return cell
 	
 func _get_increased_defect_stat(stat_value) -> float : 
-	return stat_value + IVCellCreator.max_stat_value * 0.15
+	return stat_value + IVCellCreator.max_stat_value * 0.20
 	
 	
