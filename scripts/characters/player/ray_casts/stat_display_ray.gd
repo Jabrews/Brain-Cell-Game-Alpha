28d5591a1f50dbbf589412c1cb2f9042c3a7ac01
Jacob <brewbakerjacob@gmail.com>
2026-09-23 21:39:59 -0400

    fixed stat display not dissapearing (again)

--- scripts/characters/player/ray_casts/stat_display_ray.gd
+++ scripts/characters/player/ray_casts/stat_display_ray.gd
@@ -1,27 +1,37 @@
-extends RayCast3D 
+extends RayCast3D
 
-var selected_cell_stat_display : Area3D = null
-@onready var player_parent : CharacterBody3D = $"../../../.."
+var selected_cell_stat_display: Area3D = null
+@onready var player_parent: CharacterBody3D = $"../../../.."
 
 
-func _process(_delta):
+func _process(_delta: float) -> void:
+	var new_stat_display: Area3D = null
 
 	if is_colliding():
-		var collider = get_collider()
-		
-		if collider :
-			
-			if selected_cell_stat_display : 
-				if collider != selected_cell_stat_display :
-					selected_cell_stat_display.toggle_display_stat_area(false, null)
-					selected_cell_stat_display = null
-				
-			
+		var collider := get_collider()
 
+		if collider and collider is Area3D:
 			if collider.is_in_group("stat_display_area"):
-				selected_cell_stat_display = collider
-				selected_cell_stat_display.toggle_display_stat_area(true, player_parent)
-	else:
-		if selected_cell_stat_display:
-			selected_cell_stat_display.toggle_display_stat_area(false, null)
-			selected_cell_stat_display = null
+				new_stat_display = collider
+
+
+	# Nothing changed
+	if new_stat_display == selected_cell_stat_display:
+		return
+
+
+	# Turn off old display
+	if selected_cell_stat_display:
+		selected_cell_stat_display.toggle_display_stat_area(false, null)
+
+
+	# Store new display
+	selected_cell_stat_display = new_stat_display
+
+
+	# Turn on new display
+	if selected_cell_stat_display:
+		selected_cell_stat_display.toggle_display_stat_area(
+			true,
+			player_parent
+		)
