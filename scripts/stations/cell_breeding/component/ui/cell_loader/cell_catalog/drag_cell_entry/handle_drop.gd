ee4d75a5bcc3a1349f715051a738364b0ae84005
Jacob <brewbakerjacob@gmail.com>
2026-09-23 21:05:36 -0400

    increased radius in which we accept drop on box

--- scripts/stations/cell_breeding/component/ui/cell_loader/cell_catalog/drag_cell_entry/handle_drop.gd
+++ scripts/stations/cell_breeding/component/ui/cell_loader/cell_catalog/drag_cell_entry/handle_drop.gd
@@ -11,44 +11,99 @@ extends Node
 @onready var left_breeding_view_boost_box : Control = $"../../../CellLoader/BreedingView/Boxes/LeftBreedingViewBoostBox"
 @onready var right_breeding_view_boost_box : Control = $"../../../CellLoader/BreedingView/Boxes/RightBreedingViewBoostBox"
 
-func _handle() :
-	var loaded_cell : BrainCell = parent_drag_cell_entry.loaded_cell 
+
+# extra detection area around each box
+@export_category("Drop Detection Radius")
+
+@export var selected_view_radius : float = 20.0
+
+@export var left_main_radius : float = 20.0
+@export var right_main_radius : float = 20.0
+
+@export var left_boost_radius : float = 20.0
+@export var right_boost_radius : float = 20.0
+
+
+func _handle() -> void:
+	var loaded_cell : BrainCell = parent_drag_cell_entry.loaded_cell
+	
+	var drag_rect : Rect2 = display_background.get_global_rect()
+	
 	
 	# selected view box
-	if display_background.get_global_rect().intersects(
-		selected_view_box.display_background.get_global_rect()
-	) :
+	if drag_rect.intersects(
+		_get_increased_rect(
+			selected_view_box.display_background,
+			selected_view_radius
+		)
+	):
 		selected_view_box._handle_entry_dropped(loaded_cell)
 	
+	
 	# left main box
-	elif display_background.get_global_rect().intersects(
-		left_breeding_view_main_box.display_background.get_global_rect()
-	) :
-		left_breeding_view_main_box._handle_entry_dropped(loaded_cell, true, true)
-		
+	elif drag_rect.intersects(
+		_get_increased_rect(
+			left_breeding_view_main_box.display_background,
+			left_main_radius
+		)
+	):
+		left_breeding_view_main_box._handle_entry_dropped(
+			loaded_cell,
+			true,
+			true
+		)
+	
+	
 	# right main box
-	elif display_background.get_global_rect().intersects(
-		right_breeding_view_main_box.display_background.get_global_rect()
-	) :
-		right_breeding_view_main_box._handle_entry_dropped(loaded_cell, true, true)
+	elif drag_rect.intersects(
+		_get_increased_rect(
+			right_breeding_view_main_box.display_background,
+			right_main_radius
+		)
+	):
+		right_breeding_view_main_box._handle_entry_dropped(
+			loaded_cell,
+			true,
+			true
+		)
+	
 	
 	# left boost box
-	elif display_background.get_global_rect().intersects(
-		left_breeding_view_boost_box.display_background.get_global_rect()
-	) :
-		left_breeding_view_boost_box._handle_entry_dropped(loaded_cell, true, true)
+	elif drag_rect.intersects(
+		_get_increased_rect(
+			left_breeding_view_boost_box.display_background,
+			left_boost_radius
+		)
+	):
+		left_breeding_view_boost_box._handle_entry_dropped(
+			loaded_cell,
+			true,
+			true
+		)
+	
 	
 	# right boost box
-	elif display_background.get_global_rect().intersects(
-		right_breeding_view_boost_box.display_background.get_global_rect()
-	) :
-		right_breeding_view_boost_box._handle_entry_dropped(loaded_cell, true, true)
+	elif drag_rect.intersects(
+		_get_increased_rect(
+			right_breeding_view_boost_box.display_background,
+			right_boost_radius
+		)
+	):
+		right_breeding_view_boost_box._handle_entry_dropped(
+			loaded_cell,
+			true,
+			true
+		)
 	
-	# default drop sound insues
-	else : 
-		GLBreedingComponetsBus.emit_signal('breeder_play_sound','dropped')
 	
-		
-		
-		
-	
+	# default drop sound
+	else:
+		GLBreedingComponetsBus.emit_signal(
+			'breeder_play_sound',
+			'dropped'
+		)
+
+
+func _get_increased_rect(control : Control, radius : float) -> Rect2:
+	var global_rect : Rect2 = control.get_global_rect()
+	return global_rect.grow(radius)
